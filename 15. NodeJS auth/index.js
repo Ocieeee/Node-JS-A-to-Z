87add36ba0e0dad3194867c36b43ee3d914c7605
Ocieeee <omchudhari0003@gmil.com>
2025-09-01 20:07:34 +0530

    mm

--- 15. NodeJS auth/index.js
+++ 15. NodeJS auth/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db.js");
 const authRoutes = require("./routes/auth-routes.js");
+const homeRoutes = require("./routes/home-routes.js");
+const adminRoutes = require("./routes/admin-routes.js");
 
 connectToDB();
 const app = express();
@@ -10,6 +12,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
+app.use("api/home", homeRoutes);
+app.use("api/admin", adminRoutes);
 
 app.listen(PORT, () => {
   console.log("Server listeinnig to PORT", PORT);
